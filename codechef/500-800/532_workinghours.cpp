d905e5052e9a514746b02f6bd877cb210c7df5d2
rishit <168338952+rishitbhanot1@users.noreply.github.com>
2026-02-13 20:22:11 +0530

    #cc_532_workinghours

---
+++ codechef/500-800/532_workinghours.cpp
@@ -0,0 +1,24 @@
+/*
+Problem: Chef in his Office
+Platform: CodeChef
+Rating: 532
+
+Concept:
+Direct arithmetic calculation
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<(4*x) + y<<"\n";
+    }
+    return 0;
+}
