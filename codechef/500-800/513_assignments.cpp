b9e44d4ac313e6192941a3f4033ae71a932aedf2
rishit <168338952+rishitbhanot1@users.noreply.github.com>
2026-02-13 20:20:26 +0530

    #cc_513_assignments

---
+++ codechef/500-800/513_assignments.cpp
@@ -0,0 +1,24 @@
+/*
+Problem: Janmansh and Assignments
+Platform: CodeChef
+Rating: 513
+
+Concept:
+Simple time arithmetic
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    cout << (x>7 ? "No\n":"Yes\n");
+	}
+	return 0;
+}
