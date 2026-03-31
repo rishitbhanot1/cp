aef78ffc407dde396b778c508fc6a2368294a222
rishit <168338952+rishitbhanot1@users.noreply.github.com>
2026-03-31 22:23:17 +0530

    #cc_592_sharktank

---
+++ codechef/500-800/592_sharktank.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(10*a<5*b)
+	        cout<<"SECOND"<<endl;
+	    else if (10*a>5*b)
+	        cout<<"FIRST"<<endl;
+	    else
+	        cout<<"ANY"<<endl;
+	}
+    return 0;
+}
