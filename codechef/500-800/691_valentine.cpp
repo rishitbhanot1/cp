99ed8849bb62bcd3e5014a7438474e61f1ab7040
rishit <168338952+rishitbhanot1@users.noreply.github.com>
2026-04-03 23:00:44 +0530

    #cc_691_valentine

---
+++ codechef/500-800/691_valentine.cpp
@@ -0,0 +1,14 @@
+// general question on floor function
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<floor(x/y)<<endl;
+    }
+    return 0;
+}
