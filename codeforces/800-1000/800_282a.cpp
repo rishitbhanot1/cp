5d6c3b54a87936d91cab176e8f3501c9774974c9
rishit <168338952+rishitbhanot1@users.noreply.github.com>
2026-04-16 23:43:52 +0530

    #cf_800_282a

---
+++ codeforces/800-1000/800_282a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x = 0;
+    string s;
+    cin >> n;
+    while (n--) {
+        cin >> s;
+        x += (s[1] == '+' ? 1 : -1);
+    }
+    cout << x;
+}
